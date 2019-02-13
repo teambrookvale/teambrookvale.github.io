057485a83afbf6416c9d06ab553173a685ab5469
Lóránd Balog <lorandb@teambrookvale.com.au>
2019-02-14 00:10:55 +0100

    Implemented last modified filter. Removed jekyll-sitemap. Extended logic with last modified date.

---
+++ _plugins/lastmodifiedfilter.rb
@@ -0,0 +1,9 @@
+module Jekyll
+    module LastModifiedFilter
+      def file_date(input)
+        File.mtime(input)
+      end
+    end
+  end
+  
+  Liquid::Template.register_filter(Jekyll::LastModifiedFilter)
