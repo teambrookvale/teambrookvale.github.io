b5ab8456d91132934adac77a19bb10edf3d47b8f
Ed Saunders <eds@teambrookvale.com.au>
2023-06-09 11:44:05 +1000

    Add timeout to batch file so that results can be seen

--- deploy-jekyll-teambrookvale.bat
+++ deploy-jekyll-teambrookvale.bat
@@ -1,1 +1,2 @@
 ssh -i %USERPROFILE%\.ssh\ec2-key-pair.pem ubuntu@teambrookvale.com.au "cd /home/ubuntu/teambrookvale.github.io && git checkout production && git pull  && ./deploy-jekyll-teambrookvale.sh"
+timeout 20
