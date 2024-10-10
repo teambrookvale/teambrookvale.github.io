b390edee3672a508edb1ac4b31d59d57de94a99c
Ed Saunders <eds@teambrookvale.com.au>
2024-10-11 09:53:38 +1000

    Deploy script

--- deploy-jekyll-teambrookvale.sh
+++ deploy-jekyll-teambrookvale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Ruby Gems to ~/gems
-rsync --recursive --progress --delete /var/www/teambrookvale.com.au/html/ /home/ubuntu/teambrookvale.github.io/_site_backup/ 
+rsync --recursive --delete /var/www/teambrookvale.com.au/html/ /home/ubuntu/teambrookvale.github.io/_site_backup/ 
 export GEM_HOME="$HOME/gems"
 export PATH="$HOME/gems/bin:$PATH"
 cd /home/ubuntu/teambrookvale.github.io
@@ -9,5 +9,5 @@ git checkout production
 git pull
 rm -rf ./_site
 JEKYLL_ENV=production jekyll build
-sudo rsync --recursive --progress --delete --delete-excluded --exclude 'deploy-jekyll-teambrookvale.sh' --exclude 'Jenkinsfile' --exclude 'README.md' /home/ubuntu/teambrookvale.github.io/_site/ /var/www/teambrookvale.com.au/html/
+sudo rsync --recursive --delete --delete-excluded --exclude 'deploy-jekyll-teambrookvale.sh' --exclude 'Jenkinsfile' --exclude 'README.md' /home/ubuntu/teambrookvale.github.io/_site/ /var/www/teambrookvale.com.au/html/
 sudo chown -R www-data:www-data /var/www/teambrookvale.com.au/
